2071bd9f90330a67b3df75354ca0bca74d204d6c
Charlie-UWG <charlie.shimonishi.uwg@gmail.com>
2021-03-09 07:32:47 +0900

    tailwind.css flex justify 使用してみる

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <ul className="inline-flex p-2 ">
-          <li>Menu1</li>
+        <ul className="flex justify-center p-2 ">
+          <li className=" f- pr-2 bg-pink-300">Menu1</li>
           <li>Menu2</li>
           <li>Menu3</li>
         </ul>
       </Head>
       <main>
-        <h1 className="font-bold text-6xl mt-5 ml-auto mr-auto" >Hello!</h1>
+        <h1 className="font-bold text-6xl mt-5 text-center" >Hello!</h1>
       </main>
     </div>
   )
